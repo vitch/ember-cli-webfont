55e083742909082fe0640a5ff0d4f74a9ecfe044
Kelvin Luck <git@kelvinluck.com>
2015-07-02 11:31:51 +0100

    Manually generating the directory we want in the default blueprint for the addon.

--- blueprints/ember-cli-webfont/index.js
+++ blueprints/ember-cli-webfont/index.js
@@ -1,5 +1,12 @@
 /* jshint node: true */
+var fs = require('fs');
 
 module.exports = {
-  normalizeEntityName: function() {}
+  normalizeEntityName: function() {},
+  afterInstall: function() {
+    var path = this.project.root + '/app/webfont-svg';
+    if (!fs.existsSync(path)) {
+      fs.mkdirSync(path);
+    }
+  }
 };
